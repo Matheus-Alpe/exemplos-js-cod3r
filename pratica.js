3a458e4924f45f8e4378a3247c96154816fa8b6d
Matheus Alves <matttalves@gmail.com>
2020-08-11 19:40:58 -0300

    2020/08/11 19:40

--- pratica.js
+++ pratica.js
@@ -6,14 +6,56 @@ const pessoa = {
   }
 }
 
-const gen = pessoa.definirNome('Matheus')
-console.log(gen.next().value)
-console.log(gen.next().value)
-console.log(gen.next().value)
-console.log(gen.next().value)
+const gen = pessoa.definirNome('Matheus');
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
 
-console.log('\n\n\n')
+console.log('\n\n\n');
 
-let cc = 'Matheus alves'
-console.log(cc.slice(-2))
-console.log(cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4))
+let cc = 'Matheus alves';
+console.log(cc.slice(-2));
+console.log(cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4));
+
+
+let isLoggedIn = false;
+let isPaid = false
+// multi ternários
+const status = isPaid ? 'is Paid' : isLoggedIn ? 'is Logged' : 'not Logged';
+console.log(status);
+
+const items = [
+  { name: 'Bike', price: 100, solded: false },
+  { name: 'TV', price: 150, solded: true },
+  { name: 'Album', price: 130, solded: false },
+  { name: 'Phone', price: 110, solded: true },
+  { name: 'Computer', price: 50, solded: true },
+  { name: 'Keyboard', price: 10, solded: false }
+];
+
+const foundItem = items.map(({ name, solded }) => {
+  return { name, solded };
+});
+
+const filteredItem = items.filter(({ price, solded }) => {
+  return price > 100 && solded
+})
+
+const sumPriceItem = items.reduce((a, { price }) => {
+  return a += price;
+}, 0);
+
+console.log({ foundItem });
+console.log({ filteredItem });
+console.log({ sumPriceItem });
+
+setTimeout(() => {
+  console.log('Segundo');
+}, 0);
+
+setTimeout(() => {
+  console.log('Primeiro');
+}, 0)
+
+console.log('Terceiro')
