d387f4ee1775be20b7299667a397f02518ab353a
Matheus Alves <matttalves@gmail.com>
2021-01-10 23:15:03 -0300

    add: projeto galeria bootstrap/jquery/webpack

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// Arquivos SASS - Webpack Monitorar
+import './scss/index.scss'
+
+// Dependências - Webpack
+import 'jquery'
+import 'bootstrap'
+
+// Arquivos JS - WebpackMonitorar
+import './js/core/includes'
+import './js/plugins/cityButtons'
