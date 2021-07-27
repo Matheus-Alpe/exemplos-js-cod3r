499cb633143590b6c6f82112c7ca59fcf7033f0e
Matheus Alves Pereira <matttalves@gmail.com>
2021-07-26 23:17:10 -0300

    feat: added vue calculator.

---
+++ vue/calculator/src/components/Button.vue
@@ -0,0 +1,65 @@
+<template>
+    <button class="button"
+        :class="{ double, triple, operation }">
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String
+        },
+        
+        operation: {
+            type: Boolean
+        },
+        
+        double: {
+            type: Boolean
+        },
+
+        triple: {
+            type: Boolean
+        }
+    }
+}
+</script>
+
+<style>
+:root {
+    --bg-btn: #f0f0f0;
+    --border-btn: solid 1px #888;
+}
+
+.button {
+    all: unset;
+    color: #000;
+    font-size: 1.4em;
+    background-color: var(--bg-btn);
+    border-right: var(--border-btn);
+    border-bottom: var(--border-btn);
+}
+
+.button:active {
+    background-color: #ccc;
+}
+
+.button.double {
+    grid-column: span 2;
+}
+
+.button.triple {
+    grid-column: span 3;
+}
+
+.button.operation {
+    background-color: #fa8231;
+    color: #fff;
+}
+
+.button.operation:active {
+    background-color: #fa8231cc;
+}
+</style>
